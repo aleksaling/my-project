3645babbe87a459760d5aac1a65bca2b74df21da
Aleksandra Tiukova <alextyukova@gmail.com>
2022-01-09 02:25:56 +0300

    Calc

---
+++ JS/Week13/js.js
@@ -0,0 +1,26 @@
+function GetName () {
+    alert (`Привет ${prompt ("Как тебя зовут?")}`);
+}
+
+
+
+function sum(){
+    let a = prompt (`Введите первое число`);
+    let b = prompt (`Введите второе число`);
+    alert (+a+ +b);
+}
+function min(){
+    let a = prompt (`Введите первое число`);
+    let b = prompt (`Введите второе число`);
+    alert (a-b);
+}
+function mult(){
+    let a = prompt (`Введите первое число`);
+    let b = prompt (`Введите второе число`);
+    alert (a*b);
+}
+function div(){
+    let a = prompt (`Введите первое число`);
+    let b = prompt (`Введите второе число`);
+    alert (a/b);
+}
