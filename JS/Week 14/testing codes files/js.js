e1e7efb325e04f503b1b75917317634672f4f1fe
Aleksandra Tiukova <alextyukova@gmail.com>
2022-01-19 21:11:01 +0300

    HomeWork fixes

--- JS/Week 14/testing codes files/js.js
+++ JS/Week 14/testing codes files/js.js
@@ -1,3 +1,8 @@
-function click (){
+function CleanInput (){
+    document.getElementsByTagName('input')[0].value = "";
+}   
+
+function clickMeGoogle () {
+    window.location.href = 'http://www.google.com';
     document.getElementsByTagName('input')[0].value = "";
 }
